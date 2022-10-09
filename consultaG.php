14d94dd95a90458b1fbfc1a488896bc4c193c38a
David Cabrera <yermandad@gmail.com>
2022-10-08 23:29:26 -0500

    consultaG.php

--- consultaG.php
+++ consultaG.php
@@ -66,8 +66,6 @@
               
 
         </div>
-
-
         </form>
         <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
         <script src="ajax.js"></script>
